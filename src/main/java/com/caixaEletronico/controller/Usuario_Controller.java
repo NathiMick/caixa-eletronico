3d50341ffa3e7372670bc4fc708cc8dff528222f
Jessica Marques <83045525+jessica-mss@users.noreply.github.com>
2021-09-21 01:10:36 -0300

    Desenvolvimento Usuario - rotas PUT e DELETE
    Desenvolvimento Usuario - rotas PUT e DELETE

--- src/main/java/com/caixaEletronico/controller/Usuario_Controller.java
+++ src/main/java/com/caixaEletronico/controller/Usuario_Controller.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +45,16 @@ public class Usuario_Controller {
 	@PostMapping(value="/cadastrar")
 	public ResponseEntity<Usuario> Post (@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+	
+	@PutMapping("/alterar")
+	public ResponseEntity<Usuario> Put (@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public void delete (@PathVariable long id) {
+		repository.deleteById(id);
+		//COLOCAR MENSAGEM DE "DELETADO COM SUCESSO"
 	}
 }
